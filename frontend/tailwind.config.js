cccc61410fde97678d1633d43654fa958af8ef04
kinho <kinhong99@gmail.com>
2021-05-27 15:42:10 -0400

    added hidden scrollbar utility

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,34 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Poppins"],
+        elokint: ["Cinzel"],
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        /* Hide scrollbar for Chrome, Safari and Opera */
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none",
+        },
+
+        /* Hide scrollbar for IE, Edge and Firefox */
+        ".no-scrollbar": {
+          "-ms-overflow-style": "none" /* IE and Edge */,
+          "scrollbar-width": "none" /* Firefox */,
+        },
+      };
+      addUtilities(newUtilities);
+    }),
+  ],
+};
