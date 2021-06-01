39a17c9cca3fefebb373e5b46423ebe44973699a
kinho <kinhong99@gmail.com>
2021-06-01 16:12:40 -0400

    backend-db connection setup

---
+++ backend/db.js
@@ -0,0 +1,23 @@
+// pool instance and dotenv configuration
+const Pool = require("pg").Pool;
+require("dotenv").config();
+
+// development configuration
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+};
+
+// production configuration
+const productionConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? devConfig : productionConfig
+);
+
+module.exports = pool;
