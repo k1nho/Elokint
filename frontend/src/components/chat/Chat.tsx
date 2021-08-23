f81e57191c786d5d0867b8e09622dc9a472eaa87
k1nho <kinhong99@gmail.com>
2021-08-23 09:37:19 -0400

    change imports to bracket format of const exports

--- frontend/src/components/chat/Chat.tsx
+++ frontend/src/components/chat/Chat.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
-import MessageBubble from "./MessageBubble";
+import {MessageBubble} from "./MessageBubble";
 
 const socket = io("http://localhost:4000", { transports: ["websocket"] });
 
