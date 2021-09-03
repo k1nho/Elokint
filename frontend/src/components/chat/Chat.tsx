8f71e035a47b17d0300dc6a29f012b57e6f029b4
kinho <kinhong99@gmail.com>
2021-09-03 14:58:45 -0400

    start of UI toggle button for API connection

--- frontend/src/components/chat/Chat.tsx
+++ frontend/src/components/chat/Chat.tsx
@@ -17,6 +17,7 @@ export const Chat: React.FC<Iprops> = () => {
   const [message, setMessage] = useState("");
   const [chatLog, setChatLog] = useState<Message[]>([]);
   const [user, setUser] = useState("");
+  const [elokintMode, setElokintMode] = useState(false);
   const socket = useRef<any>(null);
 
   const getUser = async () => {
@@ -27,7 +28,7 @@ export const Chat: React.FC<Iprops> = () => {
       });
       const parseResponse = await response.json();
       setUser(parseResponse.user_name);
-    } catch (err) {
+    } catch (err: any) {
       console.error(err.message);
     }
   };
@@ -83,7 +84,13 @@ export const Chat: React.FC<Iprops> = () => {
     e.preventDefault();
     //send chat message of an user to the server
     if (message !== "") {
-      socket.current.emit("chatMessage", message, user);
+      // handle autocompletion
+      if (elokintMode) {
+        console.log("On elokint mode");
+        socket.current.emit("chatMessage", message, user);
+      } else {
+        socket.current.emit("chatMessage", message, user);
+      }
       setMessage("");
     }
   };
@@ -214,27 +221,54 @@ export const Chat: React.FC<Iprops> = () => {
             onChange={(e) => setMessage(e.currentTarget.value)}
             className=" w-full  px-4 py-2 rounded-md outline-none focus:ring-2 focus:ring-yellow-500 font-body"
           />
-
-          <button
-            className=" bg-yellow-500 text-white focus:outline-none font-bold font-body  rounded-md px-4 py-2 uppercase text-sm hover:bg-yellow-600 flex items-center space-x-1 transition duration-300 ease-in-out"
-            type="submit"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+          <div className="flex justify-items items-center space-x-2">
+            <div
+              onClick={() => setElokintMode(!elokintMode)}
+              className={
+                elokintMode
+                  ? "bg-yellow-500 relative inline-flex flex-shrink-0 h-10 w-16 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-400"
+                  : "bg-gray-200 relative inline-flex flex-shrink-0 h-10 w-16 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-400"
+              }
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-              />
-            </svg>
-            <h1>Send</h1>
-          </button>
+              <span
+                aria-hidden="true"
+                className={
+                  elokintMode
+                    ? "translate-x-6 pointer-events-none inline-block h-9 w-9 rounded-full bg-gray-700 shadow transform ring-0 transition ease-in-out duration-200"
+                    : "translate-x-0 pointer-events-none inline-block h-9 w-9 rounded-full bg-gray-700 shadow transform ring-0 transition ease-in-out duration-200"
+                }
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className=" h-9 w-9 rounded-full"
+                  viewBox="0 0 24 24"
+                  fill=" #f59e0b"
+                >
+                  <path d="M17.944 5c-1.138 0-2.376.129-3.394.491-2.283.828-2.792.838-5.103 0-1.015-.362-2.256-.491-3.392-.491-1.971 0-4.17.387-6.055.878v1.789c.847.255 1.068.627 1.203 1.493.381 2.443 1.255 4.84 5.068 4.84 3.037 0 4.051-2.259 4.722-4.345.341-1.06 1.663-1.087 2.009-.015.673 2.089 1.682 4.36 4.725 4.36 3.814 0 4.689-2.397 5.069-4.841.135-.866.356-1.237 1.204-1.492v-1.789c-1.887-.491-4.085-.878-6.056-.878zm-7.682 3.814c-.518 2.174-1.36 4.186-3.991 4.186-3.301 0-3.974-1.903-4.275-4.973-.072-.747.092-1.04.221-1.195.947-1.134 5.952-1.088 7.611-.092.475.285.783.601.434 2.074zm11.74-.787c-.301 3.07-.975 4.973-4.275 4.973-2.629 0-3.472-2.012-3.989-4.186-.351-1.473-.042-1.789.434-2.074 1.665-1 6.667-1.038 7.611.092.129.156.293.449.219 1.195zm-4.838-1.121c1.539-.234 3.318-.03 3.791.537.104.124.234.358.176.956-.031.316-.067.616-.112.9-.41-1.487-1.457-2.283-3.855-2.393zm-14.184 2.393c-.045-.284-.082-.584-.113-.9-.058-.598.073-.832.177-.956.474-.567 2.253-.771 3.792-.537-2.398.11-3.445.906-3.856 2.393zm16.02 7.764c-1.15 2.869-6.031 2.166-7 .369-.97 1.797-5.85 2.5-7-.369.578.506 1.565.669 2.318.559 2.22-.325 2.042-2.423 3.594-2.423.425 0 .81.177 1.088.464.278-.287.662-.464 1.087-.464 1.552 0 1.375 2.099 3.594 2.423.753.11 1.74-.053 2.319-.559z" />
+                </svg>
+              </span>
+            </div>
+            <button
+              className=" bg-yellow-500 text-white focus:outline-none font-bold font-body  rounded-md px-4 py-2 uppercase text-sm hover:bg-yellow-600 flex items-center space-x-1 transition duration-300 ease-in-out"
+              type="submit"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                />
+              </svg>
+              <h1>Send</h1>
+            </button>
+          </div>
         </form>
       </div>
     </div>
