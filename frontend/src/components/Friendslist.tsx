5570594c80ad5e5f99c1d534aeb686b01d571224
k1nho <kinhong99@gmail.com>
2021-09-09 09:05:03 -0400

    friend list UI

--- frontend/src/components/Friendslist.tsx
+++ frontend/src/components/Friendslist.tsx
@@ -5,13 +5,26 @@ interface Iprops {
   authSetter: (isUserAuth: boolean) => void;
 }
 
-export const Friendslist: React.FC<Iprops> = () => {
+export const Friendslist: React.FC<Iprops> = () => 
+{
+  let friends : String[] = [" Tom", "Jerry" , "Aang", "Makima"]
   return (
     <div className="min-h-screen bg-elokint-light-black flex items-center justify-center">
-      <div>
-        <h1>Friends List</h1>
-        <div> Tom</div>
-        <div>Jerry</div>
+      <div className ="flex flex-col items-center justify-items space-y-4">
+        {friends.map((friend, i)=> {
+          return(
+          <div key = {i} className=" flex items-center bg-white rounded w-80 h-20">
+            {/* Profile picture */}
+            <div className = "flex rounded-full w-20 h-20 bg-gray-500">
+             Picture 
+            </div>
+              {/* name */}
+            <div className ="flex justify-self-center ">
+              {friend}
+
+            </div>
+          </div>
+)})}
       </div>
     </div>
   );
